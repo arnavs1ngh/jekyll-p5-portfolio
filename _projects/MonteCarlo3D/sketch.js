a8c721955bd0a354fbd6e0285084ab9703e39b34
arnavs1ngh <81491528+arnavs1ngh@users.noreply.github.com>
2021-04-15 16:45:29 +0100

    Update sketch.js

--- _projects/MonteCarlo3D/sketch.js
+++ _projects/MonteCarlo3D/sketch.js
@@ -1,7 +1,9 @@
-var x = 100,
-  y = 100,
-  angle1 = 0.0,
-  segLength = 50;
+
+var r = 250;
+var points = [];
+let rate = 20;
+
+let inPoints = 0;
 
 
 function setup() {
@@ -10,24 +12,57 @@ function setup() {
 }
 
 function draw() {
-  strokeWeight(20.0);
-  stroke(255, 100);
-  background(50);
+  // let camX = map(mouseX,0,width,-800,800)
+  camY = map(mouseY,0,height,0,1)
+  eyeZ = (height/2)/tan(PI/6)
 
-  dx = mouseX - x;
-  dy = mouseY - y;
-  angle1 = atan2(dy, dx);
-  x = mouseX - (cos(angle1) * segLength);
-  y = mouseY - (sin(angle1) * segLength);
+  //print(camY)
+  let speed = 0.005;
 
-  segment(x, y, angle1);
-  ellipse(x, y, 20, 20);
-}
+  let posx = 2*sin(frameCount*speed)
+  let posz = -2*cos(frameCount*speed)
+  //print(posx)
 
-function segment(x, y, a) {
-  push();
-  translate(x, y);
-  rotate(a);
-  line(0, 0, segLength, 0);
-  pop();
+  camera(tan(PI/6)*height*posx,-tan(PI/6)*height*0,posz*(height)*tan(PI/6),0,0,0,0,1,0)
+  perspective(camY*PI/3.0, width/height, eyeZ/10.0, eyeZ*10.0)
+
+  background(155);
+  stroke(0);
+  rectMode(CENTER);
+
+  Est_PI = ((inPoints/points.length)*6).toPrecision(17);
+  stroke(255);
+//   text("WHERE IS THIS TEXT BRUH", 300, 300);
+  // text(Est_PI, 5, height-60);
+  // text("3.141592653579323846264", 5, height-40)
+
+  for (var i = 0; i < rate; i++) {
+  var rand = createVector(random(-r, r), random(-r, r),random(-r,r));
+  //point(rand.x,rand.y,rand.z)
+  points.push(rand)
+  }
+
+  strokeWeight(5);
+  for (var i = 0; i < points.length; i++) {
+    if (abs(points[i]) <= r) {
+      stroke(255,0,0)
+      point(points[i].x,points[i].y,points[i].z)
+      inPoints++
+    }
+    else{
+      stroke(0)
+      point(points[i].x,points[i].y,points[i].z)
+    }
+  }
+  strokeWeight(1);
+
+  //stroke((rand.x/r)*255, (rand.y/400)*255, (rand.x+rand.y))
+  
+
+  translate(0,0,0)
+  fill(255,0,0,100);
+  sphere(r,24);
+  fill(255,255,255,100)
+  box(2*r)
+ 
 }
