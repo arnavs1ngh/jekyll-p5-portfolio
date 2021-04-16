65c28c0831a1de98acfbc81f626728f973f990ed
arnavs1ngh <81491528+arnavs1ngh@users.noreply.github.com>
2021-04-16 14:02:26 +0100

    Update planet.js

--- _projects/GravSim/planet.js
+++ _projects/GravSim/planet.js
@@ -40,7 +40,7 @@ class Planet {
     // this.vel = createVector(0.1,0)
     this.vel = v
     this.acc = createVector(0, 0);
-    this.r = 4;
+    this.r = 2;
     this.lifetime = 255;
     this.mass = m
   }
