28e7560e53f52593d385a3e9afe9225d77a33cbe
arnavs1ngh <81491528+arnavs1ngh@users.noreply.github.com>
2021-04-16 13:34:09 +0100

    Add files via upload

---
+++ _projects/GravSim/p5project.md
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2578
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 ---\
+layout: post\
+title:  \'93Maintenance\'94\
+date:   2020-04-15\
+---\
+\
+This page is currently undergoing some minor maintenance, some sketches and projects may seem incomplete or with rendering bugs. <br>\
+I\'92m just updating the pages, with new formatting right now. It\'92ll be up to spec again soon!\
+\
+Thanks\
+}
---
+++ _projects/GravSim/sketch.js
@@ -0,0 +1,54 @@
+
+let R = 200;
+let planets = [];
+let slider;
+let n = 8
+let a = 0.4
+
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+	background(60);
+
+	// slider = createSlider(1,100,4,1);
+	// slider.position(10,10);
+
+	// let n = slider.value();
+	vel = createVector(0,0)
+	planets.push(new Planet((windowWidth/2),(windowHeight/2),vel,100))
+
+	for (var i = 0; i < n; i++) {
+		vel = createVector(a*cos(2*PI*i/n + PI/2), a*sin(2*PI*i/n + PI/2))
+		// console.log(vel)
+		planets.push(new Planet((R*cos(2*PI*i/n)+windowWidth/2),(R*sin(2*PI*i/n)+windowHeight/2),vel,1))
+	}
+
+	
+
+}
+
+function draw() {
+	// background(60);
+
+	// let gravity  = createVector(0,0.2)
+
+	for (let a = 0; a < planets.length; a++) {
+    	for (let b = 0; b < planets.length; b++) {
+      		planets[a].calculateForce(planets[b]);
+	    }
+	}		
+	for (const planet of planets) {
+	    planet.applyForce();
+	    planet.update()
+	    planet.show();
+	  }
+		
+
+	// for (let planet of planets) {
+	//     // let gravity = createVector(0, 0.2);
+	//     // planet.applyForce(gravity);
+	//     planet.update();
+	//     planet.show();
+	// }
+
+}
+
